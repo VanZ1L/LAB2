766b1cc323265d066c74d03d2ea79304be4bdd74
VanZ1 <sniper.egorov@mail.ru>
2021-02-28 19:15:59 +0300

    Second lab

---
+++ Task1/Task1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+bool isInArea(double x, double y)
+{
+	if (x * x + y * y > 1 || (x < 0 && y < 0)) return 0;
+	else return 1;
+}
+void main()
+{
+	double x, y;
+	cout << "Enter x y\n";
+	cin >> x >> y;
+	if (isInArea(x, y) == 0) cout << "False";
+	else cout << "True";
+}
---
+++ Task2/Task2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+double f(double x)
+{
+	if (x <= 3) return x * x - 3 * x + 9;
+	else return 1 / (x * x * x + 6);
+}
+void main()
+{
+	double x;
+	cout << "x= ";
+	cin >> x;
+	cout << f(x);
+}
